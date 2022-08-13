31d828859a15f79f08be0a69d46c4023937446a9
Elena Petrova <elena_pu@mail.ru>
2022-08-13 11:18:43 +0300

    ДЗ N2, решение задачи N10

---
+++ EX010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+string ThreeDNum;
+Console.WriteLine("Введите трехзначное число: ");
+ThreeDNum=Console.ReadLine();
+if ((ThreeDNum.Length ==3) &&  (int.TryParse(ThreeDNum,out int intResult)))
+ {
+    Console.Write("Вторая цифра введенного Вами числа: ");
+    Console.WriteLine(ThreeDNum[1]);   
+ }
+else 
+ { 
+    Console.WriteLine("Проверьте правильность ввода трехзначного числа!");
+ }
