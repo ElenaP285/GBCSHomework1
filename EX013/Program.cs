3b59b039f9dce24a8c97a5c33f74c63b7454f961
Elena Petrova <elena_pu@mail.ru>
2022-08-13 12:21:25 +0300

    ДЗ N2, решение задачи N13

---
+++ EX013/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+string ThreeDNum;
+Console.WriteLine("Введите число: ");
+ThreeDNum=Console.ReadLine();
+if ((ThreeDNum.Length >=3) && (int.TryParse(ThreeDNum,out int intResult)))
+ {
+    if ((intResult<0))
+      { 
+         if (ThreeDNum.Length >=4)
+          {
+            Console.Write("Третья цифра введенного Вами числа: ");
+            Console.WriteLine(ThreeDNum[3]);
+            Environment.Exit(0);
+          } 
+          else
+          {
+            Console.WriteLine("Третьей цифры в введенном Вами числе нет, проверьте правильность ввода числа!");         
+            Environment.Exit(0);
+          }
+      }
+    Console.Write("Третья цифра введенного Вами числа: ");
+    Console.WriteLine(ThreeDNum[2]);   
+ }
+else 
+ { 
+    Console.WriteLine("Третьей цифры в введенном Вами числе нет, проверьте правильность ввода числа!");
+ }
+
