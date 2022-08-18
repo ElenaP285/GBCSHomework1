4caf645737cde4f3bc9aee599dc00635f42da3be
Elena Petrova <elena_pu@mail.ru>
2022-08-18 13:41:37 +0300

    ДЗ N3, решение задачи N19

---
+++ EX019/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+string FiveDNum;
+Console.WriteLine("Введите пятизначное число: ");
+FiveDNum=Console.ReadLine();
+if ((FiveDNum.Length ==5) &&  (int.TryParse(FiveDNum,out int intResult)) && (intResult>0) )
+ {
+    if ((FiveDNum[0]==FiveDNum[4])&&(FiveDNum[1]==FiveDNum[3]))
+     {
+       Console.WriteLine("ДА - Введенное Вами число является палиндромом.");
+     }
+    else
+     {
+       Console.WriteLine("НЕТ - Введенное Вами число НЕ является палиндромом."); 
+     }
+ }
+else 
+ { 
+    Console.WriteLine("Проверьте правильность ввода пятизначного числа!");
+ }
