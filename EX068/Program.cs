772715a888bbb4a793c4d75fc94abb3c79b43f97
Elena Petrova <elena_pu@mail.ru>
2022-09-24 14:54:29 +0300

    ДЗ N9, решение задачи N68

---
+++ EX068/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+namespace EX068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисления функции Аккермана A(m,n) с помощью рекурсии.");
+            Console.WriteLine("Введите значение m: ");
+            if (int.TryParse(Console.ReadLine(), out int mSize)&& mSize>=0)
+            { }
+            else
+            {
+                Console.WriteLine("Проверьте правильность ввода! Необходимо ввести натуральное число.");
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Введите значение n: ");
+            if (int.TryParse(Console.ReadLine(), out int nSize)&& nSize>=0)
+            { }
+            else
+            {
+                Console.WriteLine("Проверьте правильность ввода! Необходимо ввести натуральное число.");
+                Environment.Exit(0);
+            }            
+            Console.WriteLine($"Значение функции Аккермана A({mSize} , {nSize}) = " + Akkeramn(mSize, nSize));
+        }
+
+        static int Akkeramn(int m, int n)
+        {
+            if (m == 0) return n + 1;
+            if (m > 0 && n == 0) return Akkeramn(m - 1, 1);
+            if (m > 0 && n > 0) return Akkeramn(m - 1, Akkeramn(m, n - 1));
+            return Akkeramn(m, n);
+        }
+    }
+}
